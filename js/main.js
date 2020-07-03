05c55fe68b234314987f2d4a6f5397d650290275
Selena Wells <selena.elise.wells@gmail.com>
2020-07-03 11:26:48 -0400

    code working

--- js/main.js
+++ js/main.js
@@ -5,20 +5,21 @@
         //play the card match game
         //card 1 is the keyboard keycode that happens when keypress event
         let card1 = event.keyCode;
+        //take card1 (use the keycode) and find the matching audio element
         //match the audio element with the same keycode as card 1
-        let card2 = document.querySelector(`audio[data-key='${card1}']`);
 
+        let card2 = document.querySelector(`audio[data-key='${card1}']`);
+        //using the javascript string template
         //if you have a match, play card2 (matching audio element)
 
-        if (card2) { card2.play(); }
+        if (card2) {
+            card2.currentTime = 0;
+            card2.play();
+        }
 
     }
 
     window.addEventListener('keydown', playMatchingAudio);
-
-
-
-
 
 
 })();
